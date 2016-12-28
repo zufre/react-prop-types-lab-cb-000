b6ced9452bc530c06baf9905d07159d20af6ca57
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2016-12-28 12:11:40 -0800

    Updates index.js to allow for viewing in browser

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const Product = require('./components/Product');
+const React = require('react');
+const ReactDOM = require('react-dom');
 
 ReactDOM.render(
   <Product name="Dunder Mifflin" producer="PaperCo" color="white" weight={210} />,
