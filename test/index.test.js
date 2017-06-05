a08ffc0ad427baeb332828c8d70e1d71f32f50fa
Luke Ghenco <lukeghenco@gmail.com>
2017-06-05 08:57:25 -0400

    Update tests to use chai

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import sinon from 'sinon';
 
-import Product from '../components/Product';
+import Product from '../src/components/Product';
 
 const ALL_PROPS_VALID = {
   name: 'Some product',
@@ -37,97 +38,97 @@ function isRequiredPropTypeError(spy, propName) {
 }
 
 describe('<Product />', () => {
-  beforeEach(function () {
+  beforeEach(() => {
     spy = sinon.spy();
     console.error = spy;
   });
 
-  afterEach(function () {
+  afterEach(() => {
     spy.reset();
   });
 
-  describe('`name` prop', function () {
-    it('should be required', function () {
+  describe('`name` prop', () => {
+    it('should be required', () => {
       shallow(<Product {...ALL_PROPS_VALID} name={undefined} />);
-      expect(isRequiredPropTypeError(spy, 'name')).toBeTruthy(ERRORS.PROP_IS_REQUIRED);
+      expect(isRequiredPropTypeError(spy, 'name'), ERRORS.PROP_IS_REQUIRED).to.be.true;
     });
 
-    it('should have the right propType', function () {
+    it('should have the right propType', () => {
       shallow(<Product {...ALL_PROPS_VALID} />);
-      expect(spy.called).toBeFalsy(ERRORS.NOT_RIGHT_TYPE);
+      expect(spy.called, ERRORS.NOT_RIGHT_TYPE).to.be.false;
       shallow(<Product {...ALL_PROPS_VALID} name={5} />);
-      expect(isInvalidPropTypeError(spy, 'name')).toBeTruthy(ERRORS.NOT_RIGHT_TYPE);
+      expect(isInvalidPropTypeError(spy, 'name'), ERRORS.NOT_RIGHT_TYPE).to.be.true;
     });
   });
 
-  describe('`producer` prop', function () {
-    it('should be optional', function () {
+  describe('`producer` prop', () => {
+    it('should be optional', () => {
       shallow(<Product {...ALL_PROPS_VALID} producer={undefined} />);
-      expect(spy.called).toBeFalsy(ERRORS.PROP_IS_OPTIONAL);
+      expect(spy.called, ERRORS.PROP_IS_OPTIONAL).to.be.false;
     });
 
-    it('should have the right propType', function () {
+    it('should have the right propType', () => {
       shallow(<Product {...ALL_PROPS_VALID} producer="foo" />);
-      expect(spy.called).toBeFalsy(ERRORS.NOT_RIGHT_TYPE);
+      expect(spy.called, ERRORS.NOT_RIGHT_TYPE).to.be.false;
       shallow(<Product {...ALL_PROPS_VALID} producer={5} />);
-      expect(isInvalidPropTypeError(spy, 'producer')).toBeTruthy(ERRORS.NOT_RIGHT_TYPE);
+      expect(isInvalidPropTypeError(spy, 'producer'), ERRORS.NOT_RIGHT_TYPE).to.be.true;
     });
   });
 
-  describe('`hasWatermark` prop', function () {
-    it('should have a default value', function () {
-      expect(Product.defaultProps.hasWatermark).toEqual(false, 'This prop does not have a default value, or not the right one.');
+  describe('`hasWatermark` prop', () => {
+    it('should have a default value', () => {
+      expect(Product.defaultProps.hasWatermark).to.equal(false, 'This prop does not have a default value, or not the right one.');
     });
 
-    it('should be optional', function () {
+    it('should be optional', () => {
       shallow(<Product {...ALL_PROPS_VALID} hasWatermark={undefined} />);
-      expect(spy.called).toBeFalsy(ERRORS.PROP_IS_OPTIONAL);
+      expect(spy.called, ERRORS.PROP_IS_OPTIONAL).to.be.false;
     });
 
-    it('should have the right propType', function () {
+    it('should have the right propType', () => {
       shallow(<Product {...ALL_PROPS_VALID} hasWatermark={false} />);
-      expect(spy.called).toBeFalsy(ERRORS.NOT_RIGHT_TYPE);
+      expect(spy.called, ERRORS.NOT_RIGHT_TYPE).to.be.false;
       shallow(<Product {...ALL_PROPS_VALID} hasWatermark="foo" />);
-      expect(isInvalidPropTypeError(spy, 'hasWatermark')).toBeTruthy(ERRORS.NOT_RIGHT_TYPE);
+      expect(isInvalidPropTypeError(spy, 'hasWatermark'), ERRORS.NOT_RIGHT_TYPE).to.be.true;
     });
   });
 
-  describe('`color` prop', function () {
-    it('should be required', function () {
+  describe('`color` prop', () => {
+    it('should be required', () => {
       shallow(<Product {...ALL_PROPS_VALID} color={undefined} />);
-      expect(isRequiredPropTypeError(spy, 'color')).toBeTruthy(ERRORS.PROP_IS_REQUIRED);
+      expect(isRequiredPropTypeError(spy, 'color'), ERRORS.PROP_IS_REQUIRED).to.be.true;
     });
 
-    it('should only allow the right values', function () {
+    it('should only allow the right values', () => {
       shallow(<Product {...ALL_PROPS_VALID} color={'white'} />);
-      expect(spy.called).toBeFalsy('The `color` propType is not accepting a valid value.');
+      expect(spy.called, 'The `color` propType is not accepting a valid value.').to.be.false;
       shallow(<Product {...ALL_PROPS_VALID} color={'somethingelse'} />);
-      expect(spy.callCount).toEqual(1, 'The `color` propType is accepting an invalid value.');
+      expect(spy.callCount).to.equal(1, 'The `color` propType is accepting an invalid value.');
       shallow(<Product {...ALL_PROPS_VALID} color={5} />);
-      expect(spy.callCount).toEqual(2, 'The `color` propType is accepting an invalid value.');
+      expect(spy.callCount).to.equal(2, 'The `color` propType is accepting an invalid value.');
     });
   });
 
-  describe('`weight` prop', function () {
-    it('should be required', function () {
+  describe('`weight` prop', () => {
+    it('should be required', () => {
       shallow(<Product {...ALL_PROPS_VALID} weight={undefined} />);
-      expect(spy.called).toBeTruthy('The `weight` prop validator does not validate the value as being required.');
+      expect(spy.called, 'The `weight` prop validator does not validate the value as being required.').to.be.true;
     });
 
-    it('should be a number', function () {
+    it('should be a number', () => {
       shallow(<Product {...ALL_PROPS_VALID} />);
-      expect(spy.called).toBeFalsy('The `weight` prop validator does not accept a valid number.');
+      expect(spy.called, 'The `weight` prop validator does not accept a valid number.').to.be.false;
       shallow(<Product {...ALL_PROPS_VALID} weight="notanumber" />);
-      expect(spy.called).toBeTruthy('The `weight` prop validator is accepting types other than a number.');
+      expect(spy.called, 'The `weight` prop validator is accepting types other than a number.').to.be.true;
     });
 
-    it('should range between 80 and 300', function () {
+    it('should range between 80 and 300', () => {
       shallow(<Product {...ALL_PROPS_VALID} />);
-      expect(spy.called).toBeFalsy('The `weight` prop validator does not check for the weight ranging between 80 and 300.');
+      expect(spy.called, 'The `weight` prop validator does not check for the weight ranging between 80 and 300.').to.be.false;
       shallow(<Product {...ALL_PROPS_VALID} weight={10} />);
-      expect(spy.called).toBeTruthy('The `weight` prop validator does not check for the weight ranging between 80 and 300.');
+      expect(spy.called, 'The `weight` prop validator does not check for the weight ranging between 80 and 300.').to.be.true;
       shallow(<Product {...ALL_PROPS_VALID} weight={500} />);
-      expect(spy.called).toBeTruthy('The `weight` prop validator does not check for the weight ranging between 80 and 300.');
+      expect(spy.called, 'The `weight` prop validator does not check for the weight ranging between 80 and 300.').to.be.true;
     });
   });
 });
