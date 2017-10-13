0bc9afb854a4ea0e2eff82af2ea7ad7049e4d970
maxwell <github email address>
2017-10-13 10:47:04 -0400

    added stage-1, env for babel, switched root to global

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import ReactDOM from 'react-dom';
 import Product from './components/Product';
 
 ReactDOM.render(
-  <Product 
-    name="Dunder Mifflin" 
-    producer="PaperCo" 
-    color="white" 
-    weight={210} 
+  <Product
+    name="Dunder Mifflin"
+    producer="PaperCo"
+    color="white"
+    weight={210}
   />,
-  document.getElementById('root')
+  document.getElementById('global')
 );
